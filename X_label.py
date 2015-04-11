4c9b5292d43046e538b2e50db4b2404ca4ef5f88
ZhangGong <Edward@ZhangGongs-MacBook-Air.local>
2015-04-11 18:18:00 -0400

    here you go, now we have y label as well

--- X_label.py
+++ X_label.py
@@ -6,6 +6,7 @@ from object_saver import load
 from statistics import ema
 from yahoo_finance import Share
 
+
 def main():
 	# ewma = pandas.stats.moments.ewma
 	# yahoo = Share('YHOO')
@@ -23,18 +24,26 @@ def main():
 		file_object = open("raw_data.txt","w+" )
 
 		X_label = [None]*10
+		Y_label = [None]*10
 		i = 0
 		for small_g in object_groups:
 			for share in small_g.shares:
 				# print share
 				X_label[i] = []
+				Y_label[i] = []
 				data_list = small_g.shares[share].get_historical(str(sys.argv[1]), str(sys.argv[2]))
 				for index in range(len(data_list)):
 					file_object.write('Symbol: '+str(data_list[index]['Symbol'])+ ' Date : '+str(data_list[index]['Date']) + ' Volume: '+str(data_list[index]['Volume']) + ' Open : '+str(data_list[index]['Open']) + ' Close : '+str(data_list[index]['Close']) + ' High : '+str(data_list[index]['High']) + ' Low : '+str(data_list[index]['Low'])+ ' Adj_Close : '+str(data_list[index]['Adj_Close']) + "\n")
-					if( index < (len(data_list)-10) ):
+					if( index < (len(data_list)-9) ):
 						X_label[i].append([ data_list[index]['Volume'],data_list[index]['Open'],data_list[index]['Close'],data_list[index]['High'],data_list[index]['Low'],data_list[index]['Adj_Close'], ema(data_list, 10, data_list[index]['Date'])[1][0]])					
+						j = index - 1
+						if data_list[index]['Close'] <= data_list[j]['Close']:
+							Y_label[i].append(1)
+						if data_list[index]['Close'] > data_list[j]['Close']:
+							Y_label[i].append(2)
 				i=+1
 		print X_label[0]
+		print Y_label[0]
 				#print(data_list[index])
 	# data_list = yahoo.get_historical('2014-04-25', '2014-05-29')
 	except IOError:
